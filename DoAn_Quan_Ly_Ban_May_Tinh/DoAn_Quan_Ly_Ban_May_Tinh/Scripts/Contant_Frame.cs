5d446009cac67c8a4d9775ac15069e5514a0c409
noname77777777777777777 <technologyengineer7@gmail.com>
2023-10-22 23:35:35 +0700

    constant load data

---
+++ DoAn_Quan_Ly_Ban_May_Tinh/DoAn_Quan_Ly_Ban_May_Tinh/Scripts/Contant_Frame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_Quan_Ly_Ban_May_Tinh.Scripts
+{
+    public class Contant_Frame
+    {
+        public void LoadGridViewData<T>(DataGrid dataGrid, List<T> list)
+        {
+            dataGrid.DataSource = null;
+            dataGrid.DataSource = list;
+        }
+        public List<T> GetDataFromDB<T>()
+        {
+            List<T> result =  new List<T>();
+            //QLSVDBContext dbContext = new QLSVDBContext();
+
+            //if (typeof(T) == typeof(Student))
+            //{
+            //    result.AddRange(dbContext.Student.Cast<T>());
+            //}
+            return result;
+        }
+    }
+}
