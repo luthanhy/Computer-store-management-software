80c7deaeaa07988a82cb07616241e15463d23b25
LAPTOP-3URU2UOH\Hoang Ha <nervergone111@gmail.com>
2023-10-23 10:30:51 +0700

    initform

--- DoAn_Quan_Ly_Ban_May_Tinh/DoAn_Quan_Ly_Ban_May_Tinh/frm_dangnhap.Designer.cs
+++ DoAn_Quan_Ly_Ban_May_Tinh/DoAn_Quan_Ly_Ban_May_Tinh/frm_dangnhap.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DoAn_Quan_Ly_Ban_May_Tinh
 {
-    partial class Form1
+    partial class frm_dangnhap
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "frm_dangnhap";
         }
 
         #endregion
     }
 }
-
--- DoAn_Quan_Ly_Ban_May_Tinh/DoAn_Quan_Ly_Ban_May_Tinh/frm_dangnhap.cs
+++ DoAn_Quan_Ly_Ban_May_Tinh/DoAn_Quan_Ly_Ban_May_Tinh/frm_dangnhap.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DoAn_Quan_Ly_Ban_May_Tinh
 {
-    public partial class Form1 : Form
+    public partial class frm_dangnhap : Form
     {
-        public Form1()
+        public frm_dangnhap()
         {
             InitializeComponent();
         }
